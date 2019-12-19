af1cf7f90865f0bc01c969cc691f528ef0ed0805
simond983 <simond983@hotmail.com>
2019-12-19 17:46:05 +0000

    Bot setup finished

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ const bot = new Discord.Client();
 
 const token = "NjU3MjY3ODc1NDg4NDY0ODk2.Xfuw7A.A5qJc0VNfqhgH0ZlN80net2AGHg"
 
+bot.on('ready', () => {
+    console.log('Simon Trello Updates reporting for duty');
+})
+
 bot.login(token);
