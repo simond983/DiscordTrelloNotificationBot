3f7aa60fe9b580b0ee8c3c7d576471b02c227bea
simond983 <simond983@hotmail.com>
2019-12-19 17:18:55 +0000

    New  token

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const token = "NjU3MjY3ODc1NDg4NDY0ODk2.Xfuu3g.hDnA6zZ70877hF0IuI4GrUJtdxg"
+const token = "NjU3MjY3ODc1NDg4NDY0ODk2.Xfuw7A.A5qJc0VNfqhgH0ZlN80net2AGHg"
 
 bot.login(token);
